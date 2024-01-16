1549b1127fa9bd219ec786ca41d4eca2cdb02771
david0411 <tlyeyd@gmail.com>
2024-01-17 03:15:17 +0800

    create sheet

--- DBConnection_AWS.py
+++ DBConnection_AWS.py
@@ -35,31 +35,8 @@ class DBConnection_AWS:
                              "IN(SELECT FIXTURE FROM JAPJC_FIXTURES WHERE FIXTURE >= %s)")
 
         self.sql_check_fixture = "SELECT COUNT(*) FROM JAPJC_FIXTURES WHERE FIXTURE = %s"
-        # HORSE_ID, HORSE_NAME
-        self.sql_import_horse = "INSERT INTO JAPJC_HORSE VALUES(%s,%s)"
-        # 日期,場次,馬匹編號,六次近績,綵衣,馬名,負磅,騎師,檔位,練馬師,評分,評分加減,排位體重,優先參賽次序,配備
-        self.sql_import_card = ("INSERT INTO JAPJC_CARD "
-                                "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)")
-        # FIXTURE, FEATURE
-        self.sql_import_fixture = "INSERT INTO JAPJC_FIXTURES VALUES(%s,%s)"
-        # 日期,場次,投注額時間,Win,PLA,QIN,QPL,FCT,TCE,TRI,F_F,DBL,TBL,D_T,T_T,6UP
-        self.sql_import_jcpool = "INSERT INTO JAPJC_JCPOOL VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-        # 日期,賠率時間,場次,馬號,綵衣,馬名,檔位,負磅,騎師,練馬師,獨贏,位置,EMT
-        self.sql_import_jcodds = "INSERT INTO JAPJC_JCODDS VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-        # ID, 場次, 名次, 日期, 馬場跑道賽道, 途程, 場地狀況, 賽事班次, 檔位, 評分, 練馬師, 騎師,
-        # 頭馬距離, 獨贏賠率, 實際負磅, 沿途走位, 完成時間, 排位體重, 配備, 賽事重播, LAST_UPDATE
-        self.sql_import_horse_result = ("INSERT INTO JAPJC_HORSE_RESULT VALUES (%s,%s,%s,%s,%s,%s,%s,"
-                                        "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)")
-        # 日期, 時間, 途程, 賽事班次
-        self.sql_import_race_details = "INSERT INTO JAPJC_RACE_DETAILS VALUES(%s,%s,%s)"
-        # 日期, 場次, 場號, 賽事班次, 途程, 場地狀況, 賽事名稱, 馬場跑道, 馬場賽道, 時間1, 時間2, 時間3, 時間4, 時間5, 時間6,
-        # 分段時間1, 分段時間2, 分段時間3, 分段時間4, 分段時間5, 分段時間6, 分段時間7, 分段時間8, 分段時間9, 分段時間10
-        self.sql_import_race_result1 = ("INSERT INTO JAPJC_RESULT VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,"
-                                        "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)")
-        # 日期, 場次, 名次, 馬號, 馬名, 騎師, 練馬師, 負磅, 排位體重, 檔位, 頭馬距離, 沿途走位, 完成時間, 獨贏賠率
-        self.sql_import_race_result2 = "INSERT INTO JAPJC_RESULT2 VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-
-        self.sql_delete_fixture = "DELETE FROM JAPJC_FIXTURES WHERE FIXTURE>=%s"
+        self.sql_pools = "SELECT 場次, 投注額時間, WIN, PLA FROM JAPJC_JCPOOL WHERE 日期 = %s"
+        self.sql_odds = "SELECT 場次, 賠率時間, 馬號, 獨贏, 位置 FROM JAPJC_JCODDS WHERE 日期 = %s"
 
     def get_horse_list(self):
         self.c.execute(self.sql_horse_list)
@@ -116,57 +93,13 @@ class DBConnection_AWS:
         for row in self.c:
             return int(row[0])
 
-    def import_data(self, data_format, data_type, data):
-        import_sql = None
-        if data_type == 'horse':
-            import_sql = self.sql_import_horse
-        elif data_type == 'card':
-            import_sql = self.sql_import_card
-        elif data_type == 'fixture':
-            import_sql = self.sql_import_fixture
-        elif data_type == 'result1':
-            import_sql = self.sql_import_race_result1
-        elif data_type == 'result2':
-            import_sql = self.sql_import_race_result2
-        elif data_type == 'horse_result':
-            import_sql = self.sql_import_horse_result
-        elif data_type == 'jcpool':
-            import_sql = self.sql_import_jcpool
-        elif data_type == 'jcodds':
-            import_sql = self.sql_import_jcodds
+    def get_pools(self, target_date):
+        self.c.execute(self.sql_pools, [target_date])
+        return [[item[0], item[1], item[2], item[3]] for item in self.c.fetchall()]
 
-        if data_format == 'row':
-            try:
-                self.c.execute(import_sql, data)
-                self.conn.commit()
-            except ValueError as e:
-                print(e)
-                pass
-        elif data_format == 'df':
-            try:
-                for i, row in data.iterrows():
-                    self.c.execute(import_sql, tuple(row))
-                    self.conn.commit()
-            except ValueError as e:
-                print(e)
-                pass
-        else:
-            try:
-                for i, row in data.iterrows():
-                    ls = list(row)
-                    if ls[1] == '場次' or '馬季' in ls[1]:
-                        continue
-                    ls[3] = datetime.strptime(ls[3][:6] + '20' + ls[3][6:], "%d/%m/%Y")
-                    for j in range(len(ls)):
-                        if ls[j] == '--':
-                            ls[j] = ''
-                    self.c.execute(self.sql_import_horse_result, tuple(ls))
-                    self.conn.commit()
-            except Exception as e:
-                print(e)
-
-    def delete_fixture(self, start_date):
-        self.c.execute(self.sql_delete_fixture, [start_date])
+    def get_odds(self, target_date):
+        self.c.execute(self.sql_odds, [target_date])
+        return [[item[0], item[1], item[2], item[3], item[4]] for item in self.c.fetchall()]
 
     def close_connection(self):
         self.conn.close()
--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 import datetime
 from DBConnection_AWS import DBConnection_AWS
+import xlsxwriter
 
 if __name__ == '__main__':
-    # Initialize
-    action = None
-    next_rd = None
-    next_rd_date = None
-    next_rd_venue = None
-    next_rd_course = None
-    # must include
     db_connection = DBConnection_AWS()
     today = datetime.datetime.today()
-    next_rd = db_connection.get_next_rd()
-    next_rd_date, *temp, next_rd_venue = next_rd
-    next_rd_course = str(next_rd_venue).split(' ')[0]
+    next_rd_date, *temp = db_connection.get_next_rd()
+    no_of_race = db_connection.get_race_num()
+    result_pool = db_connection.get_pools(next_rd_date)
+    result_odds = db_connection.get_odds(next_rd_date)
+    # print(result_pool)
+    # Create a workbook and add a worksheet.
+    workbook = xlsxwriter.Workbook('JAPJC_Export_' + str(next_rd_date.strftime('%Y%m%d')) + '.xlsx')
+    for i in range(no_of_race):
+        worksheet = workbook.add_worksheet('R' + str(i + 1))
+
+    # # Some data we want to write to the worksheet.
+    # expenses = (
+    #     ['Rent', 1000],
+    #     ['Gas',   100],
+    #     ['Food',  300],
+    #     ['Gym',    50],
+    # )
+    #
+    # # Start from the first cell. Rows and columns are zero indexed.
+    # row = 2
+    # col = 5
+    #
+    # # Iterate over the data and write it out row by row.
+    # for item, cost in (expenses):
+    #     worksheet.write(row, col,     item)
+    #     worksheet.write(row, col + 1, cost)
+    #     row += 1
+    #
+    # # Write a total using a formula.
+    # worksheet.write(row, 0, 'Total')
+    # worksheet.write(row, 1, '=SUM(B1:B4)')
+    #
+    workbook.close()
