85422f0f8fb797948fa7e008d4c5c7e16a7ae781
david0411 <tlyeyd@gmail.com>
2024-01-24 03:28:40 +0800

    done write excel

--- Source/DBConnection_AWS.py
+++ Source/DBConnection_AWS.py
@@ -15,55 +15,19 @@ class DBConnection_AWS:
             print('Database connection error')
             print(e)
 
-        self.sql_horse_list = "SELECT HORSE_ID FROM JAPJC_HORSE"
-        self.sql_first_fixture = "SELECT MIN(FIXTURE) FROM JAPJC_FIXTURES"
-        self.sql_last_fixture = "SELECT MAX(FIXTURE) FROM JAPJC_FIXTURES"
-
-        self.sql_first_horse_record_date = "SELECT MIN(日期) FROM JAPJC_HORSE_RESULT WHERE ID = %s"
-        self.sql_last_horse_record_date = "SELECT MAX(日期) FROM JAPJC_HORSE_RESULT WHERE ID = %s"
-
-        self.sql_first_race_result_date = "SELECT MIN(日期) FROM JAPJC_RESULT2"
-        self.sql_last_race_result_date = "SELECT MAX(日期) FROM JAPJC_RESULT2"
-
-        self.sql_prev_rd = "SELECT FIXTURE, FEATURE FROM JAPJC_FIXTURES WHERE FIXTURE <= %s ORDER BY FIXTURE DESC"
         self.sql_next_rd = "SELECT FIXTURE, FEATURE FROM JAPJC_FIXTURES WHERE FIXTURE >= %s ORDER BY FIXTURE"
-
-        self.sql_next_venue = "SELECT FEATURE FROM JAPJC_FIXTURES WHERE FIXTURE >= %s ORDER BY FIXTURE"
-        self.sql_future_rd_date_list = "SELECT FIXTURE FROM JAPJC_FIXTURES WHERE FIXTURE >= %s"
-        self.sql_next_venue2 = "SELECT FEATURE FROM JAPJC_FIXTURES WHERE FIXTURE = %s"
         self.sql_race_num = ("SELECT MAX(場次) FROM JAPJC_CARD WHERE 日期 "
                              "IN(SELECT FIXTURE FROM JAPJC_FIXTURES WHERE FIXTURE >= %s)")
-
-        self.sql_check_fixture = "SELECT COUNT(*) FROM JAPJC_FIXTURES WHERE FIXTURE = %s"
         self.sql_pools = "SELECT 場次, 投注額時間, WIN, PLA FROM JAPJC_JCPOOL WHERE 日期 = %s"
         self.sql_odds = "SELECT 場次, 賠率時間, 馬號, 獨贏, 位置 FROM JAPJC_JCODDS WHERE 日期 = %s"
-
-    def get_prev_rd(self):
-        self.c.execute(self.sql_prev_rd, [datetime.now()])
-        return self.c.fetchone()
 
     def get_next_rd(self):
         self.c.execute(self.sql_next_rd, [datetime.now().date()])
         return self.c.fetchone()
 
-    def get_next_rd2(self, target_date):
-        self.c.execute(self.sql_future_rd_date_list, [target_date])
-        return [item[0] for item in self.c.fetchall()]
-
-    def get_next_venue(self):
-        self.c.execute(self.sql_next_venue)
-        for row in self.c:
-            return str(row[0])[:2]
-
-    def get_next_venue2(self, target_date):
-        self.c.execute(self.sql_next_venue2, [target_date])
-        for row in self.c:
-            return str(row[0])[:2]
-
     def get_race_num(self):
         self.c.execute(self.sql_race_num, [datetime.now().date()])
-        for row in self.c:
-            return int(row[0])
+        return self.c.fetchone()[0]
 
     def get_pools(self, target_date):
         self.c.execute(self.sql_pools, [target_date])
--- Source/main.py
+++ Source/main.py
@@ -1,42 +1,77 @@
 import datetime
-
 from Source.DBConnection_AWS import DBConnection_AWS
 import xlsxwriter
 
 if __name__ == '__main__':
     db_connection = DBConnection_AWS()
-    today = datetime.datetime.today()
     next_rd_date, *temp = db_connection.get_next_rd()
     no_of_race = db_connection.get_race_num()
     result_pool = db_connection.get_pools(next_rd_date)
     result_odds = db_connection.get_odds(next_rd_date)
-    time_list = [datetime.datetime.strptime('21:00', '%H:%M'),]
-    # print(result_pool)
+    time_list = [datetime.datetime.strptime('20:59', '%H:%M'),
+                 datetime.datetime.strptime('21:59', '%H:%M'),
+                 datetime.datetime.strptime('22:59', '%H:%M'),
+                 datetime.datetime.strptime('23:59', '%H:%M'),
+                 datetime.datetime.strptime('00:59', '%H:%M'),
+                 datetime.datetime.strptime('01:59', '%H:%M'),
+                 datetime.datetime.strptime('02:59', '%H:%M'),
+                 datetime.datetime.strptime('03:59', '%H:%M'),
+                 datetime.datetime.strptime('04:59', '%H:%M'),
+                 datetime.datetime.strptime('05:59', '%H:%M'),
+                 datetime.datetime.strptime('06:59', '%H:%M')
+                 ]
+
     # Create a workbook and add a worksheet.
     workbook = xlsxwriter.Workbook('JAPJC_Export_' + str(next_rd_date.strftime('%Y%m%d')) + '.xlsx')
     for i in range(no_of_race):
+        target_pool_win = []
+        target_pool_pla = []
+        target_odds_win = []
+        target_odds_pla = []
+        row_pool_win = 1
+        col_pool_win = 4
+        row_pool_pla = 1
+        col_pool_pla = 18
+        row_odds_win = 3
+        col_odds_win = 4
+        row_odds_pla = 3
+        col_odds_pla = 18
         worksheet = workbook.add_worksheet('R' + str(i + 1))
-
-    # # Some data we want to write to the worksheet.
-    # expenses = (
-    #     ['Rent', 1000],
-    #     ['Gas',   100],
-    #     ['Food',  300],
-    #     ['Gym',    50],
-    # )
-    #
-    # # Start from the first cell. Rows and columns are zero indexed.
-    # row = 2
-    # col = 5
-    #
-    # # Iterate over the data and write it out row by row.
-    # for item, cost in (expenses):
-    #     worksheet.write(row, col,     item)
-    #     worksheet.write(row, col + 1, cost)
-    #     row += 1
-    #
-    # # Write a total using a formula.
-    # worksheet.write(row, 0, 'Total')
-    # worksheet.write(row, 1, '=SUM(B1:B4)')
-    #
+        # Filter data
+        for item in time_list:
+            target_pool_win.append([sublist[2] for sublist in result_pool if (
+                    sublist[1].time() == item.time()) and (sublist[0] == i + 1)])
+            target_pool_pla.append([sublist[3] for sublist in result_pool if (
+                    sublist[1].time() == item.time()) and (sublist[0] == i + 1)])
+            target_odds_win.append([[sublist[2],sublist[3]] for sublist in result_odds if (
+                    sublist[1].time() == item.time()) and (sublist[0] == i + 1)])
+            target_odds_pla.append([[sublist[2],sublist[4]] for sublist in result_odds if (
+                    sublist[1].time() == item.time()) and (sublist[0] == i + 1)])
+        # Write Pool
+        for val in target_pool_win:
+            if len(val) != 0:
+                worksheet.write(row_pool_win, col_pool_win, val[0])
+                col_pool_win += 1
+            else:
+                col_pool_win += 1
+        for val in target_pool_pla:
+            if len(val) != 0:
+                worksheet.write(row_pool_pla, col_pool_pla, val[0])
+                col_pool_pla += 1
+            else:
+                col_pool_pla += 1
+        for val in target_odds_win:
+            if len(val) != 0:
+                for val1 in val:
+                    worksheet.write(row_odds_win + val1[0] - 1, col_odds_win, val1[1])
+                col_odds_win += 1
+            else:
+                col_odds_win += 1
+        for val in target_odds_pla:
+            if len(val) != 0:
+                for val1 in val:
+                    worksheet.write(row_odds_pla + val1[0] - 1, col_odds_pla, val1[1])
+                col_odds_pla += 1
+            else:
+                col_odds_pla += 1
     workbook.close()
