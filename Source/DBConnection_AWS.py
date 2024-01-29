513550d33b01051886304ce18ecd362aa7cd376c
david0411 <tlyeyd@gmail.com>
2024-01-29 21:43:10 +0800

    use the closest time

--- Source/DBConnection_AWS.py
+++ Source/DBConnection_AWS.py
@@ -17,6 +17,7 @@ class DBConnection_AWS:
 
         self.sql_next_rd = "SELECT FIXTURE, FEATURE FROM JAPJC_FIXTURES WHERE FIXTURE >= %s ORDER BY FIXTURE"
         self.sql_race_num = "SELECT MAX(場次) FROM JAPJC_CARD WHERE 日期 = %s"
+        self.sql_horse_num = "SELECT MAX(馬匹編號) FROM JAPJC_CARD WHERE  日期 = %s AND 場次 = %s"
         self.sql_pools = "SELECT 場次, 投注額時間, WIN, PLA FROM JAPJC_JCPOOL WHERE 日期 = %s"
         self.sql_odds = "SELECT 場次, 賠率時間, 馬號, 獨贏, 位置 FROM JAPJC_JCODDS WHERE 日期 = %s"
 
@@ -26,6 +27,10 @@ class DBConnection_AWS:
 
     def get_race_num(self, target_date):
         self.c.execute(self.sql_race_num, [target_date])
+        return self.c.fetchone()[0]
+
+    def get_horse_num(self, target_date, race_num):
+        self.c.execute(self.sql_horse_num, [target_date, race_num])
         return self.c.fetchone()[0]
 
     def get_pools(self, target_date):
--- Source/main.py
+++ Source/main.py
@@ -5,34 +5,21 @@ import xlsxwriter
 if __name__ == '__main__':
     db_connection = DBConnection_AWS()
     next_rd_date, *temp = db_connection.get_next_rd()
-    # no_of_race = db_connection.get_race_num(next_rd_date)
-    # result_pool = db_connection.get_pools(next_rd_date)
-    # result_odds = db_connection.get_odds(next_rd_date)
-    no_of_race = db_connection.get_race_num('2024-01-24')
-    result_pool = db_connection.get_pools('2024-01-24')
-    result_odds = db_connection.get_odds('2024-01-24')
-    # time_list = [dt.datetime.combine(next_rd_date + dt.timedelta(days=-1), dt.datetime.strptime('21:00', '%H:%M').time()),
-    #              dt.datetime.combine(next_rd_date + dt.timedelta(days=-1), dt.datetime.strptime('22:00', '%H:%M').time()),
-    #              dt.datetime.combine(next_rd_date + dt.timedelta(days=-1), dt.datetime.strptime('23:00', '%H:%M').time()),
-    #              dt.datetime.combine(next_rd_date, dt.datetime.strptime('00:00', '%H:%M').time()),
-    #              dt.datetime.combine(next_rd_date, dt.datetime.strptime('01:00', '%H:%M').time()),
-    #              dt.datetime.combine(next_rd_date, dt.datetime.strptime('02:00', '%H:%M').time()),
-    #              dt.datetime.combine(next_rd_date, dt.datetime.strptime('03:00', '%H:%M').time()),
-    #              dt.datetime.combine(next_rd_date, dt.datetime.strptime('04:00', '%H:%M').time()),
-    #              dt.datetime.combine(next_rd_date, dt.datetime.strptime('05:00', '%H:%M').time()),
-    #              dt.datetime.combine(next_rd_date, dt.datetime.strptime('06:00', '%H:%M').time()),
-    #              dt.datetime.combine(next_rd_date, dt.datetime.strptime('07:00', '%H:%M').time())]
-    time_list = [dt.datetime.combine(dt.date(2024, 1, 24) + dt.timedelta(days=-1), dt.datetime.strptime('21:00', '%H:%M').time()),
-                 dt.datetime.combine(dt.date(2024, 1, 24) + dt.timedelta(days=-1), dt.datetime.strptime('22:00', '%H:%M').time()),
-                 dt.datetime.combine(dt.date(2024, 1, 24) + dt.timedelta(days=-1), dt.datetime.strptime('23:00', '%H:%M').time()),
-                 dt.datetime.combine(dt.date(2024, 1, 24), dt.datetime.strptime('00:00', '%H:%M').time()),
-                 dt.datetime.combine(dt.date(2024, 1, 24), dt.datetime.strptime('01:00', '%H:%M').time()),
-                 dt.datetime.combine(dt.date(2024, 1, 24), dt.datetime.strptime('02:00', '%H:%M').time()),
-                 dt.datetime.combine(dt.date(2024, 1, 24), dt.datetime.strptime('03:00', '%H:%M').time()),
-                 dt.datetime.combine(dt.date(2024, 1, 24), dt.datetime.strptime('04:00', '%H:%M').time()),
-                 dt.datetime.combine(dt.date(2024, 1, 24), dt.datetime.strptime('05:00', '%H:%M').time()),
-                 dt.datetime.combine(dt.date(2024, 1, 24), dt.datetime.strptime('06:00', '%H:%M').time()),
-                 dt.datetime.combine(dt.date(2024, 1, 24), dt.datetime.strptime('07:00', '%H:%M').time())]
+    no_of_race = db_connection.get_race_num(next_rd_date)
+    result_pool = db_connection.get_pools(next_rd_date)
+    result_odds = db_connection.get_odds(next_rd_date)
+    time_list = [
+        dt.datetime.combine(next_rd_date + dt.timedelta(days=-1), dt.datetime.strptime('21:00', '%H:%M').time()),
+        dt.datetime.combine(next_rd_date + dt.timedelta(days=-1), dt.datetime.strptime('22:00', '%H:%M').time()),
+        dt.datetime.combine(next_rd_date + dt.timedelta(days=-1), dt.datetime.strptime('23:00', '%H:%M').time()),
+        dt.datetime.combine(next_rd_date, dt.datetime.strptime('00:00', '%H:%M').time()),
+        dt.datetime.combine(next_rd_date, dt.datetime.strptime('01:00', '%H:%M').time()),
+        dt.datetime.combine(next_rd_date, dt.datetime.strptime('02:00', '%H:%M').time()),
+        dt.datetime.combine(next_rd_date, dt.datetime.strptime('03:00', '%H:%M').time()),
+        dt.datetime.combine(next_rd_date, dt.datetime.strptime('04:00', '%H:%M').time()),
+        dt.datetime.combine(next_rd_date, dt.datetime.strptime('05:00', '%H:%M').time()),
+        dt.datetime.combine(next_rd_date, dt.datetime.strptime('06:00', '%H:%M').time()),
+        dt.datetime.combine(next_rd_date, dt.datetime.strptime('07:00', '%H:%M').time())]
     # Create a workbook and add a worksheet.
     workbook = xlsxwriter.Workbook('JAPJC_Export_' + str(next_rd_date.strftime('%Y%m%d')) + '.xlsx')
     for i in range(no_of_race):
@@ -48,42 +35,31 @@ if __name__ == '__main__':
         col_odds_win = 0
         row_odds_pla = 2
         col_odds_pla = 12
+        horse_num = db_connection.get_horse_num(next_rd_date, i + 1)
         worksheet = workbook.add_worksheet('R' + str(i + 1))
         # Filter data
         for item in time_list:
-            target_pool_win.append([sublist[2] for sublist in result_pool if (
-                min(sublist[1].time(), key=lambda x: abs(x - item.time()))) and (sublist[0] == i + 1)])
-            target_pool_pla.append([sublist[3] for sublist in result_pool if (
-                min(sublist[1].time(), key=lambda x: abs(x - item.time()))) and (sublist[0] == i + 1)])
-            target_odds_win.append([[sublist[2], sublist[3]] for sublist in result_odds if (
-                min(sublist[1].time(), key=lambda x: abs(x - item.time()))) and (sublist[0] == i + 1)])
-            target_odds_pla.append([[sublist[2], sublist[4]] for sublist in result_odds if (
-                min(sublist[1].time(), key=lambda x: abs(x - item.time()))) and (sublist[0] == i + 1)])
-        # Write Pool
-        for val in target_pool_win:
-            if len(val) != 0:
-                worksheet.write(row_pool_win, col_pool_win, val[0])
-                col_pool_win += 1
-            else:
-                col_pool_win += 1
-        for val in target_pool_pla:
-            if len(val) != 0:
-                worksheet.write(row_pool_pla, col_pool_pla, val[0])
-                col_pool_pla += 1
-            else:
-                col_pool_pla += 1
-        for val in target_odds_win:
-            if len(val) != 0:
-                for val1 in val:
-                    worksheet.write(row_odds_win + val1[0] - 1, col_odds_win, val1[1])
-                col_odds_win += 1
-            else:
-                col_odds_win += 1
-        for val in target_odds_pla:
-            if len(val) != 0:
-                for val1 in val:
-                    worksheet.write(row_odds_pla + val1[0] - 1, col_odds_pla, val1[1])
-                col_odds_pla += 1
-            else:
-                col_odds_pla += 1
+            worksheet.write(row_pool_win, col_pool_win,
+                            min([sublist for sublist in result_pool if sublist[0] == i + 1],
+                                key=lambda x: abs(x[1] - item))[2])
+            col_pool_win += 1
+            worksheet.write(row_pool_pla, col_pool_pla,
+                            min([sublist for sublist in result_pool if sublist[0] == i + 1],
+                                key=lambda x: abs(x[1] - item))[3])
+            col_pool_pla += 1
+            for j in range(horse_num):
+                worksheet.write(row_odds_win, col_odds_win,
+                                min([sublist for sublist in result_odds if
+                                     (sublist[0] == i + 1) and (sublist[2] == j + 1)],
+                                    key=lambda x: abs(x[1] - item))[3])
+                row_odds_win += 1
+                worksheet.write(row_odds_pla, col_odds_pla,
+                                min([sublist for sublist in result_odds if
+                                     (sublist[0] == i + 1) and (sublist[2] == j + 1)],
+                                    key=lambda x: abs(x[1] - item))[4])
+                row_odds_pla += 1
+            row_odds_win = 2
+            row_odds_pla = 2
+            col_odds_win += 1
+            col_odds_pla += 1
     workbook.close()
